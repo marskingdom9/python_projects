4f8ce17406caa9439cb56cb05c1a5e7d1914cf29
marskingdom9 <mayank_singh09@hotmail.com>
2024-07-09 13:17:41 -0700

    Added madlibs project

---
+++ madlibs/madlibs.py
@@ -0,0 +1,10 @@
+# This is an incredibly simple Mad Libs Game
+
+name = input("Name: ")
+location = input("Location: ")
+age = input("Age: ")
+idol = input("Famous Idol: ")
+
+madlib = f"My name is {name}! I am from {location} and I am currently {age} years old\nMy favorite idol is {idol}"
+
+print(madlib)
